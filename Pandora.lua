77b482acc68d5af22f042a7be90bb624d0dc405d
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2019-11-13 19:46:52 +0100

    Added sources and readme examples.

---
+++ Pandora.lua
@@ -0,0 +1,89 @@
+-- Copyright 2019 Pablo Blanco Celdrán
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy 
+-- of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights 
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+-- copies of the Software, and to permit persons to whom the Software is 
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in 
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+-- SOFTWARE.
+
+---
+-- Clones the table
+---
+local function clone(value)
+  if type(value) ~= 'table' then return value end
+  local tbl = {}
+  for k, v in pairs(value) do
+    tbl[k] = clone(v)
+  end
+  return tbl
+end
+
+---
+-- Builds the class prototype.
+---
+local function class(name)
+  return function(def)
+    local Class = {}
+    Class.__name__ = name
+    local defaultValues = {}
+    for name, value in pairs(def) do
+      if (type(name) == table and name.static) or type(value) == 'function' then
+        Class[name.static or name] = value
+      elseif type(name) == 'string' then
+        defaultValues[name] = value
+      end
+    end
+    local ctor = Class[name]
+    Class[name] = nil
+    Class.__index = Class
+    return setmetatable(Class, {
+      __call = function(_, ...)
+        local self = setmetatable(clone(defaultValues), Class)
+        ctor(self, ...)
+        return self
+      end
+    })
+  end
+end
+
+---
+-- Marks the field to not be added to the new instances but to the class itself.
+---
+local function static(name)
+  return { static = true, name = name }
+end
+
+local operators = {
+  ['=='] = '__eq',
+  ['<'] = '__lt',
+  ['<='] = '__le',
+  ['..'] = '__concat',
+  ['%'] = '__mod',
+  ['/'] = '__div',
+  ['*'] = '__mul',
+  ['-'] = '__sub',
+  ['+'] = '__add',
+  ['()'] = '__call',
+  ['string'] = '__tostring',
+  ['='] = '__newindex',
+  ['[]'] = '__index'
+}
+
+---
+-- Operator overload sugar translator
+---
+local function operator(symbol)
+  return operators[symbol] or symbol
+end
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # pandora
-Pandora is a small pure-lua object oriented library, which enhaces your lua modules with classes, in an actual class style! 
+
+Pandora (/pænˈdɔːrə/ pan-DOR-ə; Greek: Πανδώρα, from the XVII moon of Saturn) is a small pure-lua object oriented library.
+
+Pandora adds classes with the common `class` syntax to Lua, which makes you able to build classes without the need of transpilation (Like [Moonscript][moonscript], which is a great language by the way), and without the classical metatable syntax.
+
+## Example
+
+An example of a module, let this "file" be Person.lua:
+
+```lua
+-- Remember to import class and operator functions!
+----
+-- Person example
+----
+return class 'Person' {
+  ----
+  -- Name
+  -- @type {string}
+  ----
+  name = '<no name>',
+
+  ----
+  -- Constructor
+  -- @param {string} name
+  ----
+  Person = function(this, name)
+    this.name = name
+  end,
+
+  ----
+  -- Compares two persons by name
+  -- @param {string} other
+  ----
+  [operator'=='] = function(this, other)
+    return this.name == other.name
+  end,
+
+  ----
+  -- Shouts the name
+  ----
+  shout = function(this)
+    print('My name is ' ..  this.name .. '!')
+  end
+}
+```
+
+And use just like:
+
+```lua
+local Person = require 'Person'
+
+local john = Person('John')
+john:shout()
+```
+
+[moonscript]: https://moonscript.org/
